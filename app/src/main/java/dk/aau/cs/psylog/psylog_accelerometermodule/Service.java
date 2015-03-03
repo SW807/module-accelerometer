6f819554440de1985eb46ee6f6fc315c6f9ca803
Mathias Winde <Mapede11@student.aau.dk>
2015-03-03 09:36:28 +0100

    Lavet AccService om til Service

--- app/src/main/java/dk/aau/cs/psylog/psylog_accelerometermodule/Service.java
+++ app/src/main/java/dk/aau/cs/psylog/psylog_accelerometermodule/Service.java
@@ -2,7 +2,7 @@ package dk.aau.cs.psylog.psylog_accelerometermodule;
 
 import dk.aau.cs.psylog.module_lib.SuperService;
 
-public class AccService extends SuperService {
+public class Service extends SuperService {
     @Override
     public void setSensor() {
         sensor = new AccelerometerListener(this);
