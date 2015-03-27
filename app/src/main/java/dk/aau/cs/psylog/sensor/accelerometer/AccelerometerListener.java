b3ab3c557547ad27bba19090eea98f54c326bc5d
Mathias Winde <Mapede11@student.aau.dk>
2015-03-27 11:32:56 +0100

    Accelerometret gør nu brug af SldingWindow

--- app/src/main/java/dk/aau/cs/psylog/sensor/accelerometer/AccelerometerListener.java
+++ app/src/main/java/dk/aau/cs/psylog/sensor/accelerometer/AccelerometerListener.java
@@ -9,9 +9,16 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.Uri;
+import android.util.Log;
+import android.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 import dk.aau.cs.psylog.module_lib.DBAccessContract;
 import dk.aau.cs.psylog.module_lib.ISensor;
+import dk.aau.cs.psylog.module_lib.SlidingWindow;
 
 public class AccelerometerListener implements SensorEventListener, ISensor {
 
@@ -21,6 +28,9 @@ public class AccelerometerListener implements SensorEventListener, ISensor {
     private int sensorDelay;
     ContentResolver cr;
 
+    private final int MAXSIZE = 1000;
+    private final float MAXDISTANCE = 0.5f;
+
     public AccelerometerListener(Context context) {
         mSensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
@@ -28,16 +38,32 @@ public class AccelerometerListener implements SensorEventListener, ISensor {
         cr = context.getContentResolver();
     }
 
+    SlidingWindow<Float> slidingWindowX = new SlidingWindow<>(MAXDISTANCE),
+                         slidingWindowY = new SlidingWindow<>(MAXDISTANCE),
+                         slidingWindowZ = new SlidingWindow<>(MAXDISTANCE);
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
         if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            Uri uri = Uri.parse(DBAccessContract.DBACCESS_CONTENTPROVIDER + "accelerometer_accelerations");
-            ContentValues values = new ContentValues();
-            values.put("accX", sensorEvent.values[0]);
-            values.put("accY", sensorEvent.values[1]);
-            values.put("accZ", sensorEvent.values[2]);
-            cr.insert(uri, values);
+            boolean boolX = slidingWindowX.addPointAndCheck(new Pair<>(sensorEvent.values[0], new Date().getTime()));
+            boolean boolY = slidingWindowY.addPointAndCheck(new Pair<>(sensorEvent.values[1], new Date().getTime()));
+            boolean boolZ = slidingWindowZ.addPointAndCheck(new Pair<>(sensorEvent.values[2], new Date().getTime()));
+
+            if(boolX || boolY || boolZ)
+                addToDatabase(slidingWindowX.resetSlidingWindow().first,
+                              slidingWindowY.resetSlidingWindow().first,
+                              slidingWindowZ.resetSlidingWindow().first);
         }
+    }
+
+    private void addToDatabase(float x, float y, float z){
+        Uri uri = Uri.parse(DBAccessContract.DBACCESS_CONTENTPROVIDER + "accelerometer_accelerations");
+        ContentValues values = new ContentValues();
+        values.put("accX", x);
+        values.put("accY", y);
+        values.put("accZ", z);
+        cr.insert(uri, values);
+        Log.d("ACC", "Der blev uploadet til DB");
     }
 
     @Override
